1f7b72d5f5744defbc910c35ef9624925b870ca0
Laura Navarro <lauranavarrohdez@hotmail.com>
2022-10-27 19:48:14 +0200

    Script for analyze sample

--- scripts/analyse_sample.sh
+++ scripts/analyse_sample.sh
@@ -1,44 +1,45 @@
-if [ "$#" -ne 1 ]
-then
-    echo "Usage: $0 <sampleid>"
-    exit 1
-fi
 
-sampleid=$1
+export WD=$(pwd)
+cd $WD
 
-echo "Running FastQC..."
-mkdir -p out/fastqc
-fastqc -o out/fastqc data/${sampleid}_?.fastq.gz
-echo
+#FastQC
+	mkdir out/fastqc
+	fastqc -o out/fastqc data/*.fastq.gz
 
-echo "Running cutadapt..."
-mkdir -p log/cutadapt
-mkdir -p out/cutadapt
-cutadapt \
-    -m 20 \
-    -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA \
-    -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
-    -o out/cutadapt/${sampleid}_1.trimmed.fastq.gz \
-    -p out/cutadapt/${sampleid}_2.trimmed.fastq.gz data/${sampleid}_1.fastq.gz data/${sampleid}_2.fastq.gz \
-    > log/cutadapt/${sampleid}.log
-echo
+	cd $WD/data
+	mkdir original
+	mv *.fastq.gz original
 
-echo "Running STAR index..."
-mkdir -p res/genome/star_index
-STAR \
-    --runThreadN 4 \
-    --runMode genomeGenerate \
-    --genomeDir res/genome/star_index/ \
-    --genomeFastaFiles res/genome/ecoli.fasta \
-    --genomeSAindexNbases 9
-echo
+#Ecoli Genome
+	mkdir -p res/genome
+	wget -O res/genome/ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
 
-echo "Running STAR alignment..."
-mkdir -p out/star/${sampleid}
-STAR \
-    --runThreadN 4 \
-    --genomeDir res/genome/star_index/ \
-    --readFilesIn out/cutadapt/${sampleid}_1.trimmed.fastq.gz out/cutadapt/${sampleid}_2.trimmed.fastq.gz \
-    --readFilesCommand zcat \
-    --outFileNamePrefix out/star/${sampleid}/
-echo
+#Remove the adapters
+	mkdir -p out/cutadapt
+	mkdir -p log/cutadapt
+	echo "Running cutadapt..."
+		-m 20 \
+		-a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA \
+		-A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
+		-o out/cutadapt/ERR2868172_1.trimmed.fastq.gz \
+		-p out/cutadapt/ERR2868172_2.trimmed.fastq.gz \
+		data/ERR2868172_1.fastq.gz data/ERR2868172_2.fastq.gz > log/cutadapt/ERR2868172.log
+
+#Index the genome
+	mkdir -p res/genome/star_index
+	echo " Running STAR index"
+		--runThreadN 8 --runMode genomeGenerate \
+		--genomeDir res/genome/star_index/ \
+		--genomeFastaFiles res/genome/ecoli.fasta \
+		--genomeSAindexNbases 9
+
+#Align the adapter-free reads
+	mkdir -p out/star/ERR2868172
+	echo "Running STAR"
+		--runThreadN 4 --genomeDir res/genome/star_index/ \
+		--readFilesIn out/cutadapt/ERR2868172_1.trimmed.fastq.gz out/cutadapt/ERR2868172_2.trimmed.fastq.gz \
+		--readFilesCommand zcat \
+		--outFileNamePrefix out/star/ERR2868172/
+#Generate a report
+	echo "Running multiqc"
+	multiqc -o out/multiqc $WD
